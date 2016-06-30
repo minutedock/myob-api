58ec12e128210ea8e3f8391f93d5de0d041ce786
Patrick Barnes <patrick478@gmail.com>
2016-06-30 16:59:30 +1200

    Add TimeBilling Activity

---
+++ lib/myob/api/models/timebilling_activity.rb
@@ -0,0 +1,11 @@
+module Myob
+  module Api
+    module Model
+      class TimebillingActivity < Base
+        def model_route
+          'TImeBilling/Activity'
+        end
+      end
+    end
+  end
+end
