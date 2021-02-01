af0197e2e597773819f7b3be6d98f1d7d508e7da
Jared Armstrong <jared.armstrong@gmail.com>
2021-02-01 17:31:38 +1300

    Fix import

--- lib/myob-api.rb
+++ lib/myob-api.rb
@@ -21,6 +21,7 @@ require 'myob/api/models/invoice'
 require 'myob/api/models/invoice_item'
 require 'myob/api/models/invoice_timebilling'
 require 'myob/api/models/invoice_professional'
+require 'myob/api/models/invoice_service'
 
 require 'myob/api/models/timebilling_activity'
 
